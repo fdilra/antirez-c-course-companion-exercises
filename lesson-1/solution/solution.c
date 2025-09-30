74643948249d3ff55222a6303c200d4fb8dbd659
Franco D'Ilario <153423123+fdilra@users.noreply.github.com>
2025-09-30 23:33:18 +0200

    Added exercise solution to lesson 1

--- lesson-1/solution/solution.c
+++ lesson-1/solution/solution.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-
-
+/* Solution: 
+ * The code did not compile because it was missing the #include directive 
+ * for the standard I/O header <stdio.h>. This header contains the declaration 
+ * of the printf() function. Without it, the compiler does not know what printf() is.
+ */
 int main() {
-
+    printf("Hello world!");
     return 0;
 }
