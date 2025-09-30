2a0486c2a718dee9bad9c7cc09bc6434b5ad80db
Franco D'Ilario <153423123+fdilra@users.noreply.github.com>
2025-09-30 23:31:02 +0200

    Added exercise for lesson 1

--- lesson-1/exercise/exercise.c
+++ lesson-1/exercise/exercise.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-
-/* EXERCISE 1
- * 
+/* 
+ * Exercise: The following program does not compile. 
+ * 1. Try to compile it with `gcc exercise.c`.
+ * 2. Read the error message carefully.
+ * 3. Explain why the error occurs and fix the program.
+ * 4- Compile it again and check that it works.
  */
 
 int main() {
-
-    // TODO:
+    printf("Hello world!");
     return 0;
 }
