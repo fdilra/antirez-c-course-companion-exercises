409b4cae240c4f5e3c757381f7627d71e5c63006
Franco D'Ilario <franco.dilario24@gmail.com>
2025-10-01 10:30:56 +0200

    Added lesson-2

---
+++ lesson-2/exercise/README.md
@@ -0,0 +1,23 @@
+# Answers to the Questions - Lesson 2
+
+## Question 1
+
+What does the standard library header `stdio.h` contain?
+
+---
+
+## Question 2
+
+What is the purpose of `int` before `int main()`?
+
+---
+
+## Question 3
+
+Which GCC compiler flag(s) enable warnings?
+
+---
+
+## Question 4
+
+Which function is **required** to compile a C program?
---
+++ lesson-2/exercise/exercise.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* 
+ * Exercise: The following program has several problems.
+ * It should:
+ *   1. Print the sum of 10 and 20
+ *   2. Print the difference of 20 and 10
+ * 
+ * Read the error messages when compiling and fix the issues.
+ */
+
+int sum(int a, int b) {
+    // TODO: return the sum of a and b
+}
+
+// TODO: fix this function.
+int diff() {
+    return b - a;
+}
+
+int main() {
+    // TODO: declare and initialize the variables a and b correctly
+    a = 10;
+    b = 20;
+
+    // TODO: fix the format specifier in this printf() call
+    printf("The sum of 10 and 20 is: %s\n", sum(a, b));
+
+    // TODO: fix the arguments in this diff() call
+    printf("The difference of 20 and 10 is: %d\n", diff());
+    
+    return 0;
+}
