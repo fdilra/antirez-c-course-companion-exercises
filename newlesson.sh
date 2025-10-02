a4bb9efe336a3aaae33d46ccff17d09048a3e369
Franco D'Ilario <franco.dilario24@gmail.com>
2025-10-02 10:07:15 +0200

    Added lesson file structure generation script

---
+++ newlesson.sh
@@ -0,0 +1,15 @@
+# script for generating the folder and file strucure
+# for a new lesson
+mkdir lesson
+cd lesson
+
+mkdir exercise
+cd exercise
+touch exercise.c
+touch README.md
+cd ..
+
+mkdir solution
+cd solution
+touch ANSWER.md
+touch solution.c
